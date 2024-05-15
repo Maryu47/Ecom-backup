056c3e318367a51a20fbfe3fb62f4528fc0078ca
Maryu47 <billsithisack@gmail.com>
2024-05-15 16:18:53 +0700

    Add Product Image Gallery function

--- app/DataTables/ProductDataTable.php
+++ app/DataTables/ProductDataTable.php
@@ -22,7 +22,62 @@ class ProductDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'product.action')
+            ->addColumn('action', function($query){
+                $editBtn = "<a href='".route('admin.product.edit', $query->id)."' class='btn btn-primary'><i class='fas fa-edit'></i></a>";
+                $deleteBtn = "<a href='".route('admin.product.destroy', $query->id)."' class='btn btn-danger ml-1 delete-item'><i class='fas fa-trash'></i></a>";
+                $moreBtn = '<div class="dropdown dropleft d-inline">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-cog"></i>
+                </button>
+                <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 29px, 0px); top: 0px; left: 0px; will-change: transform;">
+                  <a class="dropdown-item has-icon" href="'.route('admin.product-image-gallery.index').'"><i class="far fa-heart"></i> Image Gallery</a>
+                  <a class="dropdown-item has-icon" href="#"><i class="far fa-file"></i> Another action</a>
+                  <a class="dropdown-item has-icon" href="#"><i class="far fa-clock"></i> Something else here</a>
+                </div>
+              </div>';
+
+                return $editBtn.$deleteBtn.$moreBtn;
+            })
+            ->addColumn('image', function($query){
+                return "<img width='70px' src = '".asset($query->thumb_image)."'></img>";
+            })
+            ->addColumn('type', function($query){
+                switch ($query->product_type) {
+                    case 'new_arrival':
+                        return '<i class = "badge badge-success">New Arrival</i>';
+                        break;
+                    case 'featured_product':
+                        return '<i class = "badge badge-warning">Featured Product</i>';
+                        break;
+                    case 'top_product':
+                        return '<i class = "badge badge-info">Top Product</i>';
+                        break;
+                    case 'best_product':
+                        return '<i class = "badge badge-danger">Best Product</i>';
+                        break;
+                    
+                    default:
+                        return '<i class = "badge badge-dark">None</i>';
+                        break;
+                }
+                
+            })
+            ->addColumn('status', function($query){
+                if ($query->status == 1) {
+                    
+                    $button = '<label class="custom-switch mt-2">
+                    <input type="checkbox" checked name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status">
+                    <span class="custom-switch-indicator"></span>   
+                  </label>';
+                } else {
+                    $button = '<label class="custom-switch mt-2">
+                    <input type="checkbox" name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status">
+                    <span class="custom-switch-indicator"></span>   
+                  </label>';
+                }
+                return $button;
+            })
+            ->rawColumns(['image', 'type', 'status', 'action'])
             ->setRowId('id');
     }
 
@@ -62,15 +117,18 @@ class ProductDataTable extends DataTable
     public function getColumns(): array
     {
         return [
+            
+            Column::make('id'),
+            Column::make('image'),
+            Column::make('name'),
+            Column::make('price'),
+            Column::make('type')->width('150'),
+            Column::make('status'),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
-                  ->width(60)
+                  ->width(250)
                   ->addClass('text-center'),
-            Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
         ];
     }
 
