be8be7ae9b57499a6ac85b1434c8c24ceff9fa6f
Maryu47 <billsithisack@gmail.com>
2024-06-14 09:38:22 +0700

    Add checkout

---
+++ resources/views/frontend/pages/payment.blade.php
@@ -0,0 +1,7 @@
+@extends('frontend.layouts.master')
+@section('title')
+    {{$settings->site_name}} || Flash Sale
+@endsection
+@section('content')
+    Payment page
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\CheckOutController;
+use App\Http\Controllers\Backend\PaymentController;
 use App\Http\Controllers\Frontend\CartController;
 use App\Http\Controllers\Frontend\FlashSaleController;
 use App\Http\Controllers\Frontend\FrontProductController;
@@ -57,6 +59,15 @@ Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user', 'as' => 
     Route::get('profile', [UserProfileController::class, 'index'])->name('profile');
     Route::put('profile', [UserProfileController::class, 'updateProfile'])->name('profile.update');
     Route::post('profile', [UserProfileController::class, 'updatePassword'])->name('profile.update.password');
+
     //User Address Routes
     Route::resource('address', UserAddressController::class);
+
+    //Checkout route
+    Route::get('checkout', [CheckOutController::class, 'index'])->name('checkout');
+    Route::post('checkout/address-create', [CheckOutController::class, 'createAddress'])->name('checkout.address.create');
+    Route::post('checkout/form-submit', [CheckOutController::class, 'checkOutFormSubmit'])->name('checkout.form-submit');
+
+    //Payment route
+    Route::get('payment', [PaymentController::class, 'index'])->name('payment');
 });
